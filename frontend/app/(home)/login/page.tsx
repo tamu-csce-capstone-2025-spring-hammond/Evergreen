d821708211007da0908cfdb4368274738069b658
CreeKeem <388cjk@gmail.com>
2025-02-27 15:16:40 -0600

    changed function names

--- frontend/app/(home)/login/page.tsx
+++ frontend/app/(home)/login/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from 'next/link';
 
 
-export default function Home() {
+export default function Login() {
   return (
     <div>
       {/* Navbar */}
--- frontend/app/(home)/signup/page.tsx
+++ frontend/app/(home)/signup/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from 'next/link';
 
 
-export default function Home() {
+export default function Signup() {
   return (
     <div>
       {/* Navbar */}
