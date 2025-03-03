276a6401a1e260bb67150d19127cc79949909b28
CreeKeem <388cjk@gmail.com>
2025-03-02 20:48:51 -0600

    initialized backend files for the news api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [AuthModule, ConfigModule.forRoot(), NewsModule],
 })
 export class AppModule {}
