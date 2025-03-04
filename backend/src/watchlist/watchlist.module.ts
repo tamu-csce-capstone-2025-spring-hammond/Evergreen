9582ec0b6ca8260b283232a9e5fdf8ab8d99c491
Clint Hart <clinthart21@tamu.edu>
2025-03-04 09:51:40 -0600

    Setting up watchlist

--- backend/src/watchlist/watchlist.module.ts
+++ backend/src/watchlist/watchlist.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WatchlistController } from './watchlist.controller';
 import { PrismaService } from 'src/prisma.service';
+import { WatchlistService } from './watchlist.service';
 
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, WatchlistService],
   controllers: [WatchlistController],
 })
 export class WatchlistModule {}
