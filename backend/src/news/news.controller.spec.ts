f4194f0bd9fe0e795abbb1750058d7a52d04dcb7
CreeKeem <388cjk@gmail.com>
2025-03-22 16:12:33 -0500

    fixed the backend tests

--- backend/src/news/news.controller.spec.ts
+++ backend/src/news/news.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsController } from './news.controller';
+import { NewsService } from './news.service';
 
 describe('NewsController', () => {
   let controller: NewsController;
@@ -7,6 +8,12 @@ describe('NewsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NewsController],
+      providers: [
+        {
+          provide: NewsService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<NewsController>(NewsController);
--- backend/src/news/news.service.spec.ts
+++ backend/src/news/news.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('NewsService', () => {
   let service: NewsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsService],
+      providers: [
+        NewsService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue('mockValue'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<NewsService>(NewsService);
