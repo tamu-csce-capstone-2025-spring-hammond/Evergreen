32557433d3bc7b0a16992f139540f9907f7459ee
CreeKeem <388cjk@gmail.com>
2025-03-22 00:09:15 -0500

    moved the news component content into the correct news.tsx file

--- frontend/src/components/user/news.tsx
+++ frontend/src/components/user/news.tsx
@@ -1,10 +1,48 @@
-const News = () => {
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function News() {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        fetchNews();
+    }, []);
+
+    const fetchNews = async () => {
+        const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+        if (!backendUrl) {
+            console.error("REACT_APP_BACKEND_URL is not defined");
+            return;
+        }
+
+        try {
+            const response = await fetch(`${backendUrl}news`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setNewsArticles(data);
+        } catch (error: any) {
+            console.error("Error fetching news:", error.message);
+        }
+    };
+
     return (
         <div className="p-4 bg-evergray-400">
-        
+            hi
+            {newsArticles.map((article, index) => (
+                <div key={index}>
+                </div>
+            ))}
         </div>
     );
-  };
-  
-  export default News;
-  
+}
