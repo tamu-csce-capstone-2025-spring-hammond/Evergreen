22077635d823caa1452ffb3e855aecc0b952ad30
kevster7000 <ktang147@gmail.com>
2025-04-10 15:59:40 -0500

    More style changes to watchlist

--- frontend/src/components/user/watchlist.tsx
+++ frontend/src/components/user/watchlist.tsx
@@ -4,7 +4,7 @@ import useJwtStore from "@/store/jwtStore";
 
 type WatchlistItem = {
   ticker: string;
-  name: string;
+  ticker_name: string;
   last_price: number;
   day_percent_change: number;
 };
@@ -39,37 +39,46 @@ const Watchlist = () => {
 
   return (
     <div className="h-full flex flex-col p-4">
-      <h2 className="text-evergray-500 mb-4">Watchlist</h2>
+        <h2 className="text-evergray-500 mb-4">Watchlist</h2>
 
-      <div className="flex-1 overflow-y-auto border border-evergray-600">
-        <table className="min-w-full text-sm text-left">
-          <thead className="bg-evergray-200 text-evergray-700">
-            <tr className="border-b border-evergray-600 ">
-              <th className="px-4 py-3">Symbol</th>
-              <th className="px-4 py-3">Last Price</th>
-              <th className="px-4 py-3">% Change</th>
-            </tr>
-          </thead>
-          <tbody>
-            {watchlist.map((item, index) => (
-              <tr key={index} className="border-b border-evergray-200 text-evergray-600">
-                <td className="px-4 py-2">
-                  <div className="font-semibold">{item.ticker}</div>
-                  <div className="text-xs">{item.name}</div>
-                </td>
-                <td className="px-4 py-3 font-mono">${item.last_price.toFixed(2)}</td>
-                <td className="px-4 py-3 font-medium">
-                  {item.day_percent_change >= 0 ? (
-                    <span className="text-evergreen-500">▲ {item.day_percent_change.toFixed(2)}%</span>
-                  ) : (
-                    <span className="text-everred-400">▼ {item.day_percent_change.toFixed(2)}%</span>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <div className="border border-evergray-600 flex-1 flex flex-col overflow-hidden">
+            <div className="overflow-x-auto">
+            <table className="min-w-full text-sm text-left table-fixed">
+                <thead className="bg-evergray-200 text-evergray-700">
+                    <tr className="border-b border-evergray-600 ">
+                        <th className="px-4 py-3 w-2/5 font-semibold">Symbol</th>
+                        <th className="px-2 w-3/10 font-semibold">Last Price</th>
+                        <th className="w-3/10 font-semibold">% Change</th>
+                    </tr>
+                </thead>
+            </table>
+            </div>
+
+            <div className="overflow-y-auto overflow-x-auto flex-1">
+            <table className="min-w-full text-sm text-left table-fixed">
+                <tbody>
+                {watchlist.map((item, index) => (
+                    <tr key={index} className="border-b border-evergray-200">
+                    <td className="pl-4 py-3 w-2/5">
+                        <div className="font-semibold tracking-wider text-evergray-600">{item.ticker}</div>
+                        <div className="text-xs text-evergray-500">{item.ticker_name}</div>
+                    </td>
+                    <td className="text-evergray-500 pl-4 py-3 font-mono text-lg w-3/10">
+                        ${item.last_price.toFixed(2)}
+                    </td>
+                    <td className="px-4 py-3 text-lg font-mono w-3/10">
+                        {item.day_percent_change >= 0 ? (
+                        <span className="text-evergreen-500">▲ {item.day_percent_change.toFixed(2)}%</span>
+                        ) : (
+                        <span className="text-everred-400">▼ {item.day_percent_change.toFixed(2).slice(1)}%</span>
+                        )}
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
+        </div>
     </div>
   );
 };
