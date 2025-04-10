e3a145829a333bc57866b33e39116db26b36bf92
Scpanakin <48834299+Scpanakin@users.noreply.github.com>
2025-04-09 23:29:24 -0500

    Can now fetch from the watchlist table

--- backend/src/watchlist/watchlist.type.ts
+++ backend/src/watchlist/watchlist.type.ts
@@ -1,6 +1,14 @@
+// RawWatchlistItem: Fetched from Prisma
+export type RawWatchlistItem = {
+  user_id: number;
+  ticker: string;
+  ticker_name: string; // This should match the field in your database
+};
+
+// watchlistElementType: Enriched data with dynamic values
 export type watchlistElementType = {
   ticker: string;
-  last_price: number;
-  day_percent_change: number;
-  name: string;
+  last_price: number | null;
+  day_percent_change: number | null;
+  ticker_name: string; // This matches the DB field
 };
